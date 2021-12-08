55d4c9ed0a2c77e04fa01dcef27ce17cc9270e39
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2021-12-08 18:46:24 +0700

    Document bitmask functions

--- src/bitmask.lua
+++ src/bitmask.lua
@@ -1,17 +1,22 @@
+-- copy locally for inreased performance
 local bnot, band, bor = bit.bnot, bit.band, bit.bor
 
+-- set mask(s) on a number
 local function set(x, ...)
 	return bor(x, ...)
 end
 
+-- clear a mask from a number
 local function clear(x, mask)	
 	return band(x, bnot(mask))
 end
 
+-- check the value of a mask for a number
 local function check(x, ...)
 	return band(x, ...)
 end
 
+-- the module
 return setmetatable({
 	set = set,
 	clear = clear,
