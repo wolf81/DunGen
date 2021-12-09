7730c215426a65e51c13a28202b92b0bbf730a97
Wolfgang Schreurs <wolfgang.schreurs@gmail.com>
2021-12-10 01:21:51 +0700

    Draw stairs

--- src/renderer.lua
+++ src/renderer.lua
@@ -564,25 +564,11 @@ local function drawString(image, text, x, y, font, color)
     love.graphics.setColor(1.0, 1.0, 1.0)
 end
 
---[[
-In JavaScript this method draws the image in the current context using the 
-options provided
-]]
-local function drawImage(image, sx, sy, swidth, sheight, x, y, width, height)
+local function drawImage(image, sx, sy, swidth, sheight, x, y)
     -- TODO: seems we don't need width & height params here ...
     local quad = love.graphics.newQuad(sx, sy, swidth, sheight, image)
     love.graphics.draw(image, quad, x - 0.5, y - 0.5)
 end
-
---[[
-function square_grid(a, b, c, d) {
-    a = b.cell_size;
-    var e;
-    for (e = 0; e <= b.max_x; e += a) draw_line(d, e, 0, e, b.max_y, c);
-    for (e = 0; e <= b.max_y; e += a) draw_line(d, 0, e, b.max_x, e, c);
-    return true
-}
---]]
 
 local function squareGrid(a, b, c)
     local a = b["cell_size"]
@@ -594,15 +580,6 @@ local function squareGrid(a, b, c)
         drawLine(d, 0, e, b["max_x"], e, c)
     end
 end
-
---[[
-function image_grid(a, b, c, d) {
-    if (a.grid != "none")
-        if (a.grid == "hex") hex_grid(a, b, c, d);
-        else a.grid == "vex" ? vex_grid(a, b, c, d) : square_grid(a, b, c, d);
-    return true
-}
---]]
 
 local function imageGrid(a, b, c, d)
     local grid = a["grid"]
@@ -617,20 +594,6 @@ local function imageGrid(a, b, c, d)
         end 
     end
 end
-
---[[
-function fill_image(a, b, c) {
-    var d = b.max_x,
-        e = b.max_y,
-        g = b.palette,
-        f;
-    (f = g.fill) ? fill_rect(c, 0, 0, d, e, f): fill_rect(c, 0, 0, d, e, g.black);
-    if (f = g.fill) fill_rect(c, 0, 0, d, e, f);
-    if (f = g.fill_grid) image_grid(a, b, f, c);
-    else if (f = g.grid) image_grid(a, b, f, c);
-    return true
-}
-]]
 
 local function fillImage(a, b, c)
     local d = b["max_x"]
@@ -655,16 +618,6 @@ local function fillImage(a, b, c)
     end
 end
 
---[[
-function get_color(a, b) {
-    for (; b;) {
-        if (a[b]) return a[b];
-        b = color_chain[b]
-    }
-    return "#000000"
-}
-]]
-
 local function getColor(a, b)
     while b ~= nil do
         if a[b] ~= nil then return a[b]
@@ -673,94 +626,6 @@ local function getColor(a, b)
 
     return a["black"]
 end
-
---[[
-function image_walls(a, b, c) {
-    var d = b.cell_size,
-        e = Math.floor(d / 4);
-    if (e < 3) e = 3;
-    b = b.palette;
-    var g;
-    cache_pixels(true);
-    var f;
-    for (f = 0; f <= a.n_rows; f++) {
-        var h = f * d,
-            i = h + d,
-            j;
-        for (j = 0; j <= a.n_cols; j++)
-            if (a.cell[f][j] & OPENSPACE) {
-                var k = j * d,
-                    l = k + d;
-                if (g = b.bevel_nw) {
-                    a.cell[f][j - 1] & OPENSPACE || draw_line(c, k - 1, h, k - 1, i, g);
-                    a.cell[f - 1][j] & OPENSPACE || draw_line(c, k, h - 1, l, h - 1, g);
-                    if (g = b.bevel_se) {
-                        a.cell[f][j + 1] & OPENSPACE || draw_line(c, l + 1, h + 1, l + 1, i, g);
-                        a.cell[f + 1][j] & OPENSPACE || draw_line(c, k + 1, i + 1, l, i + 1, g)
-                    }
-                } else if (g = b.wall_shading) {
-                    a.cell[f -
-                        1][j - 1] & OPENSPACE || wall_shading(c, k - e, h - e, k - 1, h - 1, g);
-                    a.cell[f - 1][j] & OPENSPACE || wall_shading(c, k, h - e, l, h - 1, g);
-                    a.cell[f - 1][j + 1] & OPENSPACE || wall_shading(c, l + 1, h - e, l + e, h - 1, g);
-                    a.cell[f][j - 1] & OPENSPACE || wall_shading(c, k - e, h, k - 1, i, g);
-                    a.cell[f][j + 1] & OPENSPACE || wall_shading(c, l + 1, h, l + e, i, g);
-                    a.cell[f + 1][j - 1] & OPENSPACE || wall_shading(c, k - e, i + 1, k - 1, i + e, g);
-                    a.cell[f + 1][j] & OPENSPACE || wall_shading(c, k, i + 1, l, i + e, g);
-                    a.cell[f + 1][j + 1] & OPENSPACE || wall_shading(c, l + 1, i + 1, l + e, i + e, g)
-                }
-                if (g = b.wall) {
-                    a.cell[f - 1][j] & OPENSPACE ||
-                        draw_line(c, k, h, l, h, g);
-                    a.cell[f][j - 1] & OPENSPACE || draw_line(c, k, h, k, i, g);
-                    a.cell[f][j + 1] & OPENSPACE || draw_line(c, l, h, l, i, g);
-                    a.cell[f + 1][j] & OPENSPACE || draw_line(c, k, i, l, i, g)
-                }
-            }
-    }
-    dump_pixels(c);
-    return true
-}
-]]
-
-local function cachePixels(flag)
-    -- body
-end
-
---[[
-function door_attr(a) {
-    var b;
-    if (a.key == "arch") b = {
-        arch: 1
-    };
-    else if (a.key == "open") b = {
-        arch: 1,
-        door: 1
-    };
-    else if (a.key == "lock") b = {
-        arch: 1,
-        door: 1,
-        lock: 1
-    };
-    else if (a.key == "trap") {
-        b = {
-            arch: 1,
-            door: 1,
-            trap: 1
-        };
-        if (/Lock/.test(a.desc)) b.lock = 1
-    } else if (a.key == "secret") b = {
-        wall: 1,
-        arch: 1,
-        secret: 1
-    };
-    else if (a.key == "portc") b = {
-        arch: 1,
-        portc: 1
-    };
-    return b
-}
-]]
 
 local function doorAttr(a)
     if a["key"] == "arch" then
@@ -779,65 +644,6 @@ local function doorAttr(a)
         return { ["arch"] = true, ["portc"] = true }
     end
 end
-
---[[
-function image_doors(a, b, c) {
-    var d = a.door,
-        e = b.cell_size,
-        g = Math.floor(e / 6),
-        f = Math.floor(e / 4),
-        h = Math.floor(e / 3);
-    b = b.palette;
-    var i = get_color(b, "wall"),
-        j = get_color(b, "door");
-    d.each(function(k) {
-        var l = k.row,
-            o = l * e,
-            p = k.col,
-            q = p * e;
-        k = door_attr(k);
-        var r = a.cell[l][p - 1] & OPENSPACE;
-        l = o + e;
-        p = q + e;
-        var m = Math.floor((o + l) / 2),
-            n = Math.floor((q + p) / 2);
-        if (k.wall) r ? draw_line(c, n, o, n, l, i) : draw_line(c, q, m, p, m, i);
-        if (k.arch)
-            if (r) {
-                fill_rect(c, n - 1, o, n + 1, o + g, i);
-                fill_rect(c, n - 1, l - g, n + 1, l, i)
-            } else {
-                fill_rect(c, q, m - 1, q + g, m + 1, i);
-                fill_rect(c,
-                    p - g, m - 1, p, m + 1, i)
-            }
-        if (k.door) r ? stroke_rect(c, n - f, o + g + 1, n + f, l - g - 1, j) : stroke_rect(c, q + g + 1, m - f, p - g - 1, m + f, j);
-        if (k.lock) r ? draw_line(c, n, o + g + 1, n, l - g - 1, j) : draw_line(c, q + g + 1, m, p - g - 1, m, j);
-        if (k.trap) r ? draw_line(c, n - h, m, n + h, m, j) : draw_line(c, n, m - h, n, m + h, j);
-        if (k.secret)
-            if (r) {
-                draw_line(c, n - 1, m - f, n + 2, m - f, j);
-                draw_line(c, n - 2, m - f + 1, n - 2, m - 1, j);
-                draw_line(c, n - 1, m, n + 1, m, j);
-                draw_line(c, n + 2, m + 1, n + 2, m + f - 1, j);
-                draw_line(c, n - 2, m + f, n + 1, m + f, j)
-            } else {
-                draw_line(c, n - f, m - 2, n - f, m + 1, j);
-                draw_line(c, n - f + 1, m + 2, n - 1, m + 2, j);
-                draw_line(c, n, m -
-                    1, n, m + 1, j);
-                draw_line(c, n + 1, m - 2, n + f - 1, m - 2, j);
-                draw_line(c, n + f, m - 1, n + f, m + 2, j)
-            }
-        if (k.portc)
-            if (r)
-                for (o = o + g + 2; o < l - g; o += 2) set_pixel(c, n, o, j);
-            else
-                for (o = q + g + 2; o < p - g; o += 2) set_pixel(c, o, m, j)
-    });
-    return true
-}
-]]
 
 local function imageDoors(a, b, c)
     local d, e = a["door"], b["cell_size"]
@@ -964,8 +770,6 @@ local function imageWalls(a, b, c)
     local e = math.max(math.floor(d / 4), 3)
     local b = b["palette"]
     
-    cachePixels(true)
-
     for f = 0, a["n_rows"] do
         local h = f * d
         local i = h + d
@@ -1048,24 +852,6 @@ local function imageWalls(a, b, c)
     end
 end
 
---[[
-function open_cells(a, b, c) {
-    var d = b.cell_size;
-    b = b.base_layer;
-    var e;
-    for (e = 0; e <= a.n_rows; e++) {
-        var g = e * d,
-            f;
-        for (f = 0; f <= a.n_cols; f++)
-            if (a.cell[e][f] & OPENSPACE) {
-                var h = f * d;
-                c.drawImage(b, h, g, d, d, h, g, d, d)
-            }
-    }
-    return true
-}
-]]
-
 local function openCells(a, b, c)
     local d = b["cell_size"]
     local b = b["base_layer"]
@@ -1133,23 +919,6 @@ function vex_grid(a, b, c, d) {
 
 --]]
 
---[[
-function base_layer(a, b) {
-    var c = new Element("canvas");
-    c.width = b.width;
-    c.height = b.height;
-    var d = c.getContext("2d"),
-        e = b.max_x,
-        g = b.max_y,
-        f = b.palette,
-        h;
-    (h = f.open) ? fill_rect(d, 0, 0, e, g, h): fill_rect(d, 0, 0, e, g, f.white);
-    if (h = f.open_grid) image_grid(a, b, h, d);
-    else if (h = f.grid) image_grid(a, b, h, d);
-    return c
-}
-]]
-
 local function baseLayer(a, b)
     local ctx = love.graphics.getCanvas()
 
@@ -1185,20 +954,6 @@ local function baseLayer(a, b)
     return image
 end
 
---[[
-function get_palette(a) {
-    var b;
-    b = a.palette ? a.palette : (style = a.map_style) ? palette[style] ? palette[style] : palette.standard : palette.standard;
-    var c;
-    if (c = b.colors) $H(c).keys().each(function(d) {
-        b[d] = c[d]
-    });
-    b.black || (b.black = "#000000");
-    b.white || (b.white = "#ffffff");
-    return b
-}
-]]
-
 local function getPalette(a)
     local b = a["palette"] ~= nil and a["palette"] or palette[a["map_style"]]
 
@@ -1219,23 +974,6 @@ local function newImage(width, height, f)
     return canvas
 end
 
---[[
-function scale_dungeon(a) {
-    var b = {
-        map_style: a.map_style,
-        grid: a.grid
-    };
-    b.cell_size = a.cell_size;
-    b.width = (a.n_cols + 1) * b.cell_size + 1;
-    b.height = (a.n_rows + 1) * b.cell_size + 1;
-    b.max_x = b.width - 1;
-    b.max_y = b.height - 1;
-    a = Math.floor(b.cell_size * 0.75);
-    b.font = a.toString() + "px sans-serif";
-    return b
-}
-]]
-
 local function scaleDungeon(dungeon, options)
     local b = {
         ["map_style"] = options["map_style"],
@@ -1251,39 +989,6 @@ local function scaleDungeon(dungeon, options)
 
     return b
 end
-
---[[
-function image_labels(a, b, c) {
-    var d = b.cell_size,
-        e = Math.floor(d / 2),
-        g = b.palette;
-    b = b.font;
-    g = get_color(g, "label");
-    var f;
-    for (f = 0; f <= a.n_rows; f++) {
-        var h;
-        for (h = 0; h <= a.n_cols; h++)
-            if (a.cell[f][h] & OPENSPACE) {
-                var i = cell_label(a.cell[f][h]);
-                if (i) {
-                    var j = f * d + e + 1,
-                        k = h * d + e;
-                    draw_string(c, i, k, j, b, g)
-                }
-            }
-    }
-    return true
-}
-
-function cell_label(a) {
-    a = a >> 24 & 255;
-    if (a == 0) return false;
-    a = String.fromCharCode(a);
-    if (!/^\w/.test(a)) return false;
-    if (/[hjkl]/.test(a)) return false;
-    return a
-}
-]]
 
 local function cellLabel(a)
     local a = tonumber(bit.band(bit.rshift(a, 24), 0xFF))
@@ -1314,22 +1019,115 @@ local function imageLabels(a, b, c)
     end
 end
 
---[[
-function image_dungeon(a) {
-    var b = scale_dungeon(a),
-        c = new_image(b.width, b.height),
-        d = get_palette(b);
-    b.palette = d;
-    d = base_layer(a, b, c);
-    b.base_layer = d;
-    fill_image(a, b, c);
-    open_cells(a, b, c);
-    image_walls(a, b, c);
-    a.door && image_doors(a, b, c);
-    image_labels(a, b, c);
-    a.stair && image_stairs(a, b, c)
-}
-]]
+local function treadList(a, b, c)
+    local d = {}
+    if b > a then
+        local a = a * c["cell"]
+        table.insert(d, a)
+        b = (b + 1) * c["cell"]
+        for a = a, b - 1, c["tread"] do
+            table.insert(d, a)
+        end
+    elseif b < a then
+        local a = (a + 1) * c["cell"]
+        table.insert(d, a)
+        b = b * c["cell"]
+        for a = a, b + 1, -(c["tread"]) do
+            table.insert(d, a)
+        end    
+    end
+
+    return d
+end
+
+local function scaleStairs(a)
+    a = {
+        ["cell"] = a,
+        ["len"] = a * 2,
+        ["side"] = math.floor(a / 2),
+        ["tread"] = math.floor(a / 20) + 2,
+        ["down"] = {},
+    }
+    for b = 0, a["len"] - 1, a["tread"] do
+        a["down"][b] = math.floor(b / a["len"] * a["side"])
+    end
+    return a
+end
+
+local function stairDim(a, b)
+    local r = nil
+
+    if a["next_row"] ~= a["row"] then
+        local c = math.floor((a["col"] + 0.5) * b["cell"]) - 1
+        local a = treadList(a["row"], a["next_row"], b)
+        local d = table.remove(a, 1)
+        r = {
+            ["xc"] = c,
+            ["y1"] = d,
+            ["list"] = a
+        }
+    else
+        local c = math.floor((a["row"] + 0.5) * b["cell"])
+        local a = treadList(a["col"], a["next_col"], b)
+        local d = table.remove(a, 1)
+        r = {
+            ["yc"] = c,
+            ["x1"] = d,
+            ["list"] = a,
+        }
+    end
+
+    r["side"] = b["side"]
+    r["down"] = b["down"]
+    return r
+end
+
+local function imageAscend(a, b, c)
+    if a["xc"] ~= nil then
+        local d = a["xc"] - a["side"]
+        local e = a["xc"] + a["side"]
+        for _, h in ipairs(a["list"]) do
+            drawLine(c, d, h, e, h, b)
+        end
+    else
+        local g = a["yc"] - a["side"]
+        local f = a["yc"] + a["side"]
+        for _, h in ipairs(a["list"]) do
+            drawLine(c, h, g, h, f, b)
+        end        
+    end
+end
+
+local function imageDescend(a, b, c)
+    if a["xc"] ~= nil then
+        local d = a["xc"]
+        for _, g in ipairs(a["list"]) do
+            local f = a["down"][math.abs(g - a["y1"])]
+            drawLine(c, d - f, g, d + f, g, b)
+        end
+    else
+        local e = a["yc"]
+        for _, g in ipairs(a["list"]) do
+            local f = a["down"][math.abs(g - a["x1"])]
+            drawLine(c, g, e - f, g, e + f, b)
+        end
+    end
+end
+
+local function imageStairs(a, b, c)
+    local a = a["stair"]
+    local d = scaleStairs(b["cell_size"])
+    local b = b["palette"]
+    local e = getColor(b, "stair")
+    for _, g in ipairs(a) do
+        local f = stairDim(g, d)
+        if g["key"] == "up" then 
+            imageAscend(f, e, c)
+        else
+            imageDescend(f, e, c)
+        end
+    end
+end
 
 local function render(dungeon, options)
     local b = scaleDungeon(dungeon, options)
@@ -1343,6 +1141,7 @@ local function render(dungeon, options)
         imageWalls(dungeon, b, c)
         imageDoors(dungeon, b, c)
         imageLabels(dungeon, b, c)
+        imageStairs(dungeon, b, c)
 
         --debugMap(dungeon, b)
     end)
